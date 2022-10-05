1b1da8882f68292fd43da9b5b99c88d92e03a15e
lucas <lucasfonsecard@icloud.com>
2022-10-05 12:14:45 -0300

    feat: use api to validate ApproveInstructions

--- hooks/useFreezeNFT.ts
+++ hooks/useFreezeNFT.ts
@@ -8,50 +8,22 @@ const delegate = new PublicKey("8x3MLK1raj1MfcyDCE1uE66Yo9crF83SjHMWJox3pcwQ")
 
 const useFreezeNFT = () => {
   const { connection } = useConnection()
-  const { publicKey: owner, signTransaction } = useWallet()
-
-  const approveDelegate = useCallback(
-    async (mintAddress: PublicKey) => {
-      const ownerAta = findAssociatedTokenAccountPda(mintAddress, owner)
-      const { blockhash, lastValidBlockHeight } =
-        await connection.getLatestBlockhash()
-      const ix = createApproveInstruction(ownerAta, delegate, owner, 1)
-      const tx = new Transaction()
-      tx.add(ix)
-      tx.feePayer = owner
-      tx.recentBlockhash = blockhash
-      const signedTx = await signTransaction(tx)
-      const approveSig = await connection.sendRawTransaction(
-        signedTx.serialize()
-      )
-      await connection.confirmTransaction(
-        { blockhash, lastValidBlockHeight, signature: approveSig },
-        "finalized"
-      )
-
-      return approveSig
-    },
-    [connection, owner, signTransaction]
-  )
-
-  const freezeDelegatedAccount = useCallback(
-    async (mintAddress: PublicKey) => {
-      const ownerAta = findAssociatedTokenAccountPda(mintAddress, owner)
-      const res = await fetch(`/api/freeze/${ownerAta}`)
-      const { status, data: freezeSig } = await res.json()
-      if (res.status !== 200) throw new Error(status)
-      return freezeSig
-    },
-    [owner]
-  )
+  const { publicKey: owner, sendTransaction } = useWallet()
 
   const approveAndFreezeNFT = useCallback(
     async (mintAddress: PublicKey) => {
-      const approveSig = await approveDelegate(mintAddress)
-      const freezeSig = await freezeDelegatedAccount(mintAddress)
+      const ownerAta = findAssociatedTokenAccountPda(mintAddress, owner)
+      // Create and run approve instruction.
+      const ix = createApproveInstruction(ownerAta, delegate, owner, 1)
+      const tx = new Transaction().add(ix)
+      const approveSig = await sendTransaction(tx, connection)
+      // Validate the transaction data and run freezeDelegatedNFT task using the API.
+      const res = await fetch(`/api/freeze/${ownerAta}?tx=${approveSig}`)
+      const { status, data: freezeSig } = await res.json()
+      if (res.status !== 200) throw new Error(status)
       return [approveSig, freezeSig]
     },
-    [approveDelegate, freezeDelegatedAccount]
+    [connection, owner, sendTransaction]
   )
 
   return { approveAndFreezeNFT }
--- pages/api/freeze/[ata].ts
+++ pages/api/freeze/[ata].ts
@@ -1,20 +1,52 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import { Connection, PublicKey } from "@solana/web3.js"
+import { Connection, PublicKey, TransactionInstruction } from "@solana/web3.js"
 import { keypairIdentity, Metaplex } from "@metaplex-foundation/js"
-import { AccountLayout } from "@solana/spl-token"
+import {
+  AccountLayout,
+  decodeApproveInstruction,
+  isApproveInstruction,
+} from "@solana/spl-token"
 import { getEnvClusterUrl } from "utils/getEnvClusterUrl"
 import { readKeypairFromPath } from "utils/readKeypairFromPath"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const connection = new Connection(getEnvClusterUrl())
+  const connection = new Connection(getEnvClusterUrl(), "confirmed")
   const delegate = readKeypairFromPath("delegate.json")
 
   try {
+    const tx = req.query.tx.toString()
+    const { transaction } = await connection.getTransaction(tx, {
+      maxSupportedTransactionVersion: 0,
+    })
+    // Make sure that the transaction consists exclusively of ApproveInstructions
+    // where the delegate address matches the public key in the keypair file.
+    if (
+      !transaction.message.compiledInstructions.every(
+        ({ accountKeyIndexes, data, programIdIndex }) => {
+          const decoded = decodeApproveInstruction(
+            new TransactionInstruction({
+              keys: accountKeyIndexes.map((i) => ({
+                pubkey: transaction.message.staticAccountKeys[i],
+                isSigner: transaction.message.isAccountSigner(i),
+                isWritable: transaction.message.isAccountWritable(i),
+              })),
+              programId: transaction.message.staticAccountKeys[programIdIndex],
+              data: Buffer.from(data),
+            })
+          )
+          return (
+            isApproveInstruction(decoded) &&
+            decoded.keys.delegate.pubkey.equals(delegate.publicKey)
+          )
+        }
+      )
+    )
+      throw new Error("Invalid ApproveInstruction found in transaction data.")
+
     const tokenAccount = await connection.getAccountInfo(
       new PublicKey(req.query.ata)
     )
     const { owner, mint } = AccountLayout.decode(tokenAccount.data)
-
     const { response } = await Metaplex.make(connection)
       .use(keypairIdentity(delegate))
       .nfts()
@@ -30,8 +62,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       data: response.signature,
     })
   } catch (err) {
+    console.log(err)
     return res.status(400).json({
-      status: "Error: " + err,
+      status: "Failed to freeze delegated NFT: " + err,
       data: null,
     })
   }
